b8cc9e8eb44cd27e1d02ad2a1383453ea95cadfd
Dmitry Sharabin <dmitrysharabin@gmail.com>
2021-10-27 13:10:22 +0300

    Day 24 — Challenge. Task 3

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,56 +7,25 @@
 
 import SwiftUI
 
-struct Title: ViewModifier {
+struct ProminentTitle: ViewModifier {
     func body(content: Content) -> some View {
         content
             .font(.largeTitle)
-            .foregroundColor(.white)
-            .padding()
-            .background(.blue)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
+            .foregroundColor(.blue)
     }
 }
 
 extension View {
-    func titleStyle() -> some View {
-        modifier(Title())
-    }
-}
-
-struct Watermark: ViewModifier {
-    var text: String
-    
-    func body(content: Content) -> some View {
-        ZStack(alignment: .bottomTrailing) {
-            content
-            
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(.black)
-        }
-    }
-}
-
-extension View {
-    func watermarked(with text: String) -> some View {
-        modifier(Watermark(text: text))
+    func title() -> some View {
+        modifier(ProminentTitle())
     }
 }
 
 struct ContentView: View {
     var body: some View {
-//        Text("Hello, world!")
-//            .modifier(Title())
-        
-//        Text("Hello, world!")
-//            .titleStyle()
-        
-        Color.blue
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Hacking with Swift")
+        Text("Hello, world!")
+//            .modifier(ProminentTitle())
+            .title()
     }
 }
 
